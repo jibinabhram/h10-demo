6ab8fc22d0a6cef10616d01f52164c65601243fd
jibin abhram <jibinabhram@gmail.com>
2025-12-12 11:43:58 +0530

    Replace nest build with tsc and update DB config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataModule } from './data/data.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: '1234',
-      database: 'sports',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
